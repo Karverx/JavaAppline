3814201035b1abbfc638749cf9267909e08921f1
karverx <karverx@gmail.com>
2020-09-11 23:56:47 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, world!");
+        int x = 10;
+        int y= x++;
+
+        System.out.println(x);
+        System.out.println(y);
+
     }
 }
