f72d05b73d0e56bf9e206d045ec710912a6cdf79
karverx <karverx@gmail.com>
2020-09-24 00:22:50 +0300

    add task11

---
+++ src/com/company/task11.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class task11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        String result = in.nextLine();
+        System.out.println("Вы ввели: "+result);
+    }
+}
