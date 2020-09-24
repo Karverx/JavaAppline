90bbf3c9a2f0d9a8d2c4cd87672a01e480d9a9e7
karverx <karverx@gmail.com>
2020-09-24 08:33:59 +0300

    add task14

---
+++ src/com/company/task14.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class task14 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число");
+        String S = in.nextLine();
+        int X = Integer.parseInt(S);
+        double Y = (double) X;
+
+        System.out.println(S);
+        System.out.println(X);
+        System.out.println(Y);
+    }
+
+}
