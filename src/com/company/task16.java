5484ed756991690a5c256f62bd4f7db10a9590a3
karverx <karverx@gmail.com>
2020-09-29 18:48:49 +0300

    update task16

--- src/com/company/task16.java
+++ src/com/company/task16.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class task16 {
     public static void main(String[] args) {
         String s;
-        try (BufferedReader reader = new BufferedReader(new FileReader("F:/test1.txt")))
+        try (BufferedReader reader = new BufferedReader(new FileReader("test.txt")))
         {
             while((s = reader.readLine()) != null){
                 System.out.println(s);
