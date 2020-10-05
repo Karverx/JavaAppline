260d3865158946dcc3603d7e1ffb482783d621a6
karverx <karverx@gmail.com>
2020-10-05 13:00:04 +0300

    update final_task3

--- src/com/company/final_task3.java
+++ src/com/company/final_task3.java
@@ -1,16 +1,25 @@
 package com.company;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 
 public class final_task3 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите курс доллара:");
 
-        try {
-            double dollarRate = in.nextDouble();
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Введите курс доллара:");
+            String str = reader.readLine();
+            str = str.replace(',', '.');
+            double dollarRate = Double.parseDouble(str);
+
             System.out.println("Введите количество рублей:");
-            double rub = in.nextDouble();
+            str = reader.readLine();
+            str = str.replace(',', '.');
+            double rub = Double.parseDouble(str);
+
             double dollars = rub / dollarRate;
             System.out.printf("Итого: %.2f долларов %n", dollars);
         } catch (Exception e) {
